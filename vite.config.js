cc9aab3dc09690fd01ea828c4d72c9319ee16f40
penspulse <penspulse@gmail.com>
2024-10-08 14:27:13 +0800

    chore: add deploy action

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { quasar, transformAssetUrls } from '@quasar/vite-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/data-generator/' : '/',
   server: {
     host: '0.0.0.0',
     port: 3000,
