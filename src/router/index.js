02456a42bc36aa7451b992344b51998c484946f4
penspulse <penspulse@gmail.com>
2024-10-08 14:40:25 +0800

    fix: update router

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 
 const router = createRouter({
   history: createWebHashHistory(),
